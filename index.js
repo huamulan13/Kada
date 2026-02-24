39251db40efeb474795180a8cbeda1fe8067f3e2
Ileanna <109030617+huamulan13@users.noreply.github.com>
2026-02-24 15:47:09 +0700

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ app.get('/',(req, res)=>{
     res.send ('hello word')
 });
 
+app.get('/say:moma', (req, res) => {
+    res.send ('ELO MOMAA!!')
+});
+
 app.get('/say:greeting', (req, res) => {
     const { greeting } = req.params;
     res.send(greeting);
