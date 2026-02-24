dbee56e03a6910879e239a68cc67f277d1c0e86c
Alvin Junior <alvinfajri200@gmail.com>
2026-02-24 12:13:37 +0700

    ke1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.get('/',(req, res)=>{
+    res.send ('hello word')
+});
+
+app.get('/say:greeting', (req, res) => {
+    const { greeting } = req.params;
+    res.send(greeting);
+});
+app.listen(3000)
